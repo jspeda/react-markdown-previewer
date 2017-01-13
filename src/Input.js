381b169191ec7fc9b70f70a6a22b0557ed21f07b
Joe Spedale <jspedale@gmail.com>
2017-01-13 16:47:43 -0500

    Initial commit.

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Input extends Component {
+  render() {
+    return (
+      <div className="input-box">
+        <textarea rows="50" cols="100">sample text here</textarea>
+      </div>
+    )
+  }
+}
+
+export default Input;
